7e6854c29bd435861af439bb60d93d58b8e158ea
MathiasVeraM <mathias.vera@udla.edu.ec>
2025-04-30 17:06:06 -0500

    Enemigo
    Se cambió el script que controla el enemigo, además se incluyeron animaciones para el ataque aunque no tiene efecto aún y que el enemigo persiga al jugador.

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -1,40 +1,85 @@
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
-    public int EnemyLife;
-    public GameObject enemy;
+    public float Speed = 2f;
+    public int EnemyLife = 100;
+    public float MinDistance = 1.5f;
+    public Transform player;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private Animator animator;
+    private bool isFacingRight = true;
+    private bool isAttacking = false;
+
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
+        float distanceToPlayer = Vector2.Distance(transform.position, player.position);
+
+        if (distanceToPlayer > MinDistance)
+        {
+            // Moverse hacia el jugador
+            animator.SetBool("isRunning", true);
+            animator.SetBool("isAttacking", false);
+            isAttacking = false;
+
+            transform.position = Vector2.MoveTowards(transform.position, player.position, Speed * Time.deltaTime);
+        }
+        else
+        {
+            // Atacar si está cerca
+            animator.SetBool("isRunning", false);
+
+            if (!isAttacking)
+            {
+                StartCoroutine(Attack());
+            }
+        }
+
+        Flip(player.position.x > transform.position.x);
         CheckLife();
+    }
+
+    System.Collections.IEnumerator Attack()
+    {
+        isAttacking = true;
+        animator.SetBool("isAttacking", true);
+
+        // Espera el tiempo de ataque (ajústalo según tu animación)
+        yield return new WaitForSeconds(0.7f);
+
+        animator.SetBool("isAttacking", false);
+        isAttacking = false;
+    }
+
+    void Flip(bool faceRight)
+    {
+        if (faceRight != isFacingRight)
+        {
+            isFacingRight = faceRight;
+            Vector3 scale = transform.localScale;
+            scale.x *= -1; // Solo invierte X
+            transform.localScale = scale;
+        }
     }
 
     public void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.gameObject.name == "Hit(Clone)")
+        if (collision.gameObject.name == "Hit(Clone)")
         {
             EnemyLife -= 20;
         }
     }
 
-    public void CheckLife()
+    void CheckLife()
     {
-        if(EnemyLife <= 0)
+        if (EnemyLife <= 0)
         {
-            Destroy(enemy, 0.4f);
-        }else
-        {
-            return;
+            Destroy(gameObject, 0.4f);
         }
     }
-
 }
